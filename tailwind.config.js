d895e1604297654bc8abc09e5ff4e072ffca99e1
ArmaVargas <armando091994@gmail.com>
2024-10-08 15:00:50 -0500

    dandole estilos a la pagina desde tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+
+      colors:{
+       'Red-d':' hsl(14, 86%, 42%)',
+       'Green-d':' hsl(159, 69%, 38%)',
+       'Rose-50': 'hsl(20, 50%, 98%)',
+       'Rose-100': 'hsl(13, 31%, 94%)',
+        'Rose-300': 'hsl(14, 25%, 72%)',
+        'Rose-400': 'hsl(7, 20%, 60%)',
+        'Rose-500': 'hsl(12, 20%, 44%)',
+        'Rose-900': 'hsl(14, 65%, 9%)'
+      },
+      fontFamily:{
+        'RedHat':["Red Hat Text", 'sans-serif']
+      }
+
+    },
   },
   plugins: [],
 }
